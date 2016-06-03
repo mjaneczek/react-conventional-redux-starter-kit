005c8b653b766f27a4515e732e57ff443c6e6874
Michał Janeczek <michal.janeczek@ymail.com>
2016-06-03 16:10:27 +0200

    First idea of dispatching actions in conventional way

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -3,11 +3,49 @@ import { routerMiddleware } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import makeRootReducer from './reducers'
 
+class CounterInteractor {
+  state = 0;
+
+  doubleAsync() {
+    setTimeout(() => { this.dispatch('counter:double') }, 500)
+  }
+
+  onIncrement() {
+    this.state = this.state + 1;
+  }
+
+  onIncrementBy100() {
+    this.state = this.state + 100;
+  }
+
+  onDouble() {
+    this.state = this.state * 2;
+  }
+}
+
 export default (initialState = {}, history) => {
   // ======================================================
   // Middleware Configuration
   // ======================================================
-  const middleware = [thunk, routerMiddleware(history)]
+
+  const conventionalReduxMiddleware = store => next => action => {
+    if (typeof action === 'string' || action instanceof String) {
+      var interactor = new CounterInteractor();
+      interactor.dispatch = store.dispatch;
+
+      var methodName = action.split(':').pop();
+
+      if(interactor[methodName]) {
+        interactor[methodName]();
+      }
+
+      return next({type: 'CONV_REDUX/' + action, interactor: interactor})
+    } else {
+      return next(action)
+    }
+  };
+
+  const middleware = [conventionalReduxMiddleware, thunk, routerMiddleware(history)]
 
   // ======================================================
   // Store Enhancers
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,10 +1,29 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
 
+function conventionalReducers(state = 0, action) {
+  if(action.type.startsWith("CONV_REDUX/")) {
+    var methodName = action.type.replace('CONV_REDUX/', '').split(':').pop();
+    methodName = 'on' + methodName.charAt(0).toUpperCase() + methodName.slice(1);
+
+    var interactor = action.interactor;
+    interactor.state = state;
+
+    if (interactor[methodName]) {
+      interactor[methodName]();
+    }
+
+    return interactor.state;
+  }
+
+  return state;
+}
+
 export const makeRootReducer = (asyncReducers) => {
   return combineReducers({
     // Add sync reducers here
     router,
+    conventionalReducers,
     ...asyncReducers
   })
 }
