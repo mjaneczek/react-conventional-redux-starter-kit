d27f58eefa13331eed69109b19d87e29c832388d
Michał Janeczek <michal.janeczek@ymail.com>
2016-06-14 04:48:32 +0200

    Change the name of example

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import classes from './Header.scss'
 
 export const Header = () => (
   <div>
-    <h1>React Redux Starter Kit</h1>
+    <h1>React Conventional Redux Starter Kit</h1>
     <IndexLink to='/' activeClassName={classes.activeRoute}>
       Home
     </IndexLink>
